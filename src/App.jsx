0c34814029c60c1b751881b119a4543c65df9ded
alinakassym <alinakassym@gmail.com>
2025-07-27 16:31:51 +0500

    feat: useRef

--- src/App.jsx
+++ src/App.jsx
@@ -2,79 +2,29 @@ import * as yup from "yup";
 import { useState, useRef } from "react";
 import "./App.css";
 
-const loginChangeSchema = yup
-  .string()
-  .matches(
-    /^[\w_]*$/,
-    "Login must contain only letters, numbers, and underscores",
-  )
-  .max(20, "Login must be no more than 20 characters long");
 
-const loginBlurSchema = yup
-  .string()
-  .min(3, "Login must be at least 3 characters long");
-
-const validateAndGetError = (value, schema) => {
-  let errorMessage = null;
-
-  try {
-    schema.validateSync(value, { abortEarly: false });
-  } catch ({ errors }) {
-    errorMessage = errors.join("\n");
-  }
-
-  return errorMessage;
-};
 
 function App() {
-  const [login, setLogin] = useState("");
-  const [loginError, setLoginError] = useState(null);
+  const [stateCounter, setStateCounter] = useState(0);
+  const refCounter = useRef(0);
 
-  const submitButtonRef = useRef(null);
-
-  const onLoginChange = ({ target }) => {
-    setLogin(target.value);
-    let error = validateAndGetError(target.value, loginChangeSchema);
-    setLoginError(error);
-    if (target.value.length === 20) {
-      submitButtonRef.current.focus();
-    }
+  const incrementRefCounter = () => {
+    refCounter.current += 1;
+    console.log("refCounter = ", refCounter.current);
   };
 
-  const onLoginBlur = () => {
-    let error = validateAndGetError(login, loginBlurSchema);
-    setLoginError(error);
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log("Submitted login:", login);
+  const incrementStateCounter = () => {
+    setStateCounter(stateCounter + 1);
+    console.log("stateCounter = ", stateCounter + 1);
   };
 
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-        <div className="error-text">
-          {loginError &&
-            loginError.split("\n").map((line, index) => (
-              <span key={index}>
-                {line}
-                <br />
-              </span>
-            ))}
-        </div>
-        <input
-          type="text"
-          name="login"
-          placeholder="Login"
-          value={login}
-          onChange={onLoginChange}
-          onBlur={onLoginBlur}
-        />
-        <button ref={submitButtonRef} type="submit" disabled={!!loginError}>
-          Submit
-        </button>
-      </form>
+      <p>refCounter: {refCounter.current}</p>
+      <button onClick={incrementRefCounter}>Прибавить refCounter</button>
+
+      <p>stateCounter: {stateCounter}</p>
+      <button onClick={incrementStateCounter}>Прибавить stateCounter</button>
     </div>
   );
 }
